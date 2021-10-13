480caab55a7d2b3237df2e7027b65764126ffb53
KumarB95 <kumarbalingal@gmail.com>
2021-10-13 23:48:27 +0530

    Added : Created linked list

--- src/main/java/MyNode.java
+++ src/main/java/MyNode.java
@@ -1,12 +1,22 @@
-public class MyNode<K> {
-    private MyNode next;
+public class MyNode<K> implements INode<K> {
+    private INode next;
     private K key;
 
-    public MyNode getNext() {
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.key=key;
+    }
+
+    public INode getNext() {
         return next;
     }
 
-    public void setNext(MyNode next) {
+    public void setNext(INode next) {
         this.next = next;
     }
 
--- src/test/java/MyNodeTest.java
+++ src/test/java/MyNodeTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 
 public class MyNodeTest {
     @Test
-    public void given3NumbersWHenLinkedSHouldPassLinkedTest() {
+    public void given3NumbersWHenLinkedShouldPassLinkedTest() {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
