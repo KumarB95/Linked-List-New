c494e05ec4844ae4f645b22d4e460fc92e77faab
KumarB95 <kumarbalingal@gmail.com>
2021-10-14 00:10:07 +0530

    Refactored : Creating link list by Appending

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -22,4 +22,23 @@ public class MyLinkedListTest {
 
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3NumbersWhenAppendShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+
+        boolean result=myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+
+        Assert.assertTrue(result);
+    }
 }
